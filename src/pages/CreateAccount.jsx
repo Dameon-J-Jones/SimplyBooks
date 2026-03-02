c9f6577cf2873cac80b0b9beac602fa5b9c49941
TwilightHaze <131008313+TwilightHaze@users.noreply.github.com>
2026-03-02 18:33:20 -0500

    Most Pages Included

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import Logo from "../components/Logo";
 import "./Page.css";
-<<<<<<< Updated upstream
-=======
 
 const CreateAccount = () => {
   const [firstName, setfirstName] = useState("");
@@ -92,101 +90,15 @@ const CreateAccount = () => {
         
         {/* Submit */}
         <button type="submit">Next</button>
->>>>>>> Stashed changes
 
-const CreateAccount = () => {
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setlastName] = useState("");
 
-<<<<<<< Updated upstream
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ firstName, lastName });
-    // TODO: send to backend, probably shouldn't put username and password in the console log
-  };
 
-  return (
-    <div className="page">
-      <Logo />
-
-      <form onSubmit={handleSubmit} className="page-form">
-        {/* fName */}
-        <input
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-          onChange={(e) => setfirstName(e.target.value)}
-          required
-        />
-
-        {/* lName */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={(e) => setlastName(e.target.value)}
-          required
-        />
-        {/* Addr1 */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {/* Addr2 */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {/* City */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {/* State */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {/* Zip */}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-
-        {/* Submit */}
-        <button type="submit">Login</button>
-
-        {/* Extra actions */}
-        <button type="button">Forgot Password</button>
-        <button type="button">Create New User</button>
+  
       </form>
     </div>
   );
 };
 
-=======
-      </form>
-        <Link to="/create-account2">
-          <button type="button" className="create-user-button">Next</button>
-        </Link>
-    </div>
-  );
-};
-
->>>>>>> Stashed changes
 export default CreateAccount;
+
+
