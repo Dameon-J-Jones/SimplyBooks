d27a7faea8f036dc2364c33715c752f1e9e84c65
Dameon J. Sabellona <145698002+Dameon-J-Jones@users.noreply.github.com>
2026-03-12 02:29:47 -0400

    Implemented Max Password Attempts

--- backend/controllers/authuserController.js
+++ backend/controllers/authuserController.js
@@ -14,16 +14,54 @@ export const loginUser = async (req, res) => {
     const user = result.rows[0];
     if (!user) return res.status(400).json({ message: "Cannot find user" });
 
-    const ok = await comparePassword(password, user.Password);
-    if (!ok) return res.status(401).json({ message: "Not Allowed" });
+    //check lockout for cooldown on password attempts/admin lockout
+    if (user.lockout_until && new Date(user.lockout_until) > new Date()) {
+      return res.status(403).json({ message: "Account temporarily locked. Please contact an administrator."})
+    }
 
-    res.json({
-      message: "Success",
+    const ok = await comparePassword(password, user.Password);
+
+    if (!ok){
+      const attempts = user.failed_attempts + 1;
+
+      if (attempts >= 3){
+        await pool.query(
+          `UPDATE "User"
+          SET failed_attempts = 0,
+            lockout_until = NOW() + INTERVAL '30 minutes'
+          WHERE "UName" = $1`,
+          [username]
+        );
+
+        return res.status(403).json({ message: "Account locked for 30 minutes." });
+      }
+
+      await pool.query(
+        `UPDATE "User"
+        SET failed_attempts = $1
+        WHERE "UName" = $2`,
+      [attempts, username]
+    );
+
+    return res.status(401).json({ message: "Incorrect password" });
+    } 
+
+    await pool.query(
+      `UPDATE "User"
+       SET failed_attempts = 0,
+           lockout_until = NULL
+       WHERE "UName" = $1`,
+      [username]
+    );
+
+    return res.json({
+      message: "Login successful",
       user: {
         username: user.UName,
         role: user.GroupID
       }
     });
+    
   } catch (err) {
     console.error("LOGIN ERROR:", err);
     res.status(500).json({ message: "Server error" });
